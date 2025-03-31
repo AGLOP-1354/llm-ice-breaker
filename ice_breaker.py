e518d44c5108306d173c5147f55ee3c3d984d21e
AGLOP-1354 <jake@jober.io>
2025-04-01 01:12:21 +0900

    linkedin 정보 요약 및 parsing

--- ice_breaker.py
+++ ice_breaker.py
@@ -4,35 +4,46 @@ from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 import os
 
+from agents.linkedin_lookup_agent import linkedin_lookup_agent
+from output_parsers import summary_parser
 from third_parties.likedin import scrape_linkedin_profile
 
-if __name__ == "__main__":
-    print("Hello Langchain")
-    load_dotenv()
+
+def ice_break_with(name: str) -> str:
+    linkedin_username = linkedin_lookup_agent(name=name)
+    linkedin_data = scrape_linkedin_profile(
+        linkedin_profile_url=linkedin_username, mock=True
+    )
 
     summary_template = """
-        given the LinkedIn information {information} about a person from I want you to create:
-        1. 한국어로 짧은 요약
-        2. 그것에 대한 2가지 재밌는 사실
-    """
+            given the LinkedIn information {information} about a person from I want you to create:
+            1. 한국어로 짧은 요약
+            2. 그것에 대한 2가지 재밌는 사실
+            \n{format_instructions}
+        """
 
     summary_prompt_template = PromptTemplate(
-        input_variables=["information"], template=summary_template
+        input_variables=["information"],
+        template=summary_template,
+        partial_variables={
+            "format_instructions": summary_parser.get_format_instructions()
+        },
     )
 
     llm = ChatOpenAI(
         temperature=0,
-        model_name="gpt-3.5-turbo",
+        model_name="gpt-4o-mini",
         openai_api_key=os.getenv("OPENAI_API_KEY"),
     )
-    # llm = ChatOllama(model="llama3.1",temperature=0)
-
-    chain = summary_prompt_template | llm | StrOutputParser()
-    linkedin_data = scrape_linkedin_profile(
-        linkedin_profile_url="https://www.linkedin.com/in/eden-marco/",
-        mock=True
-    )
+    # chain = summary_prompt_template | llm | StrOutputParser()
+    chain = summary_prompt_template | llm | summary_parser
 
     res = chain.invoke(input={"information": linkedin_data})
 
     print(res)
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    print("Ice Breaker Enter")
+    ice_break_with(name="Eden Macro")
--- third_parties/likedin.py
+++ third_parties/likedin.py
@@ -4,16 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 def scrape_linkedin_profile(linkedin_profile_url: str, mock: bool = False):
     """scrape information from LinkedIn profiles,
     Manually scrape the information from the LinkedIn profile"""
 
     if mock:
-        linkedin_profile_url = "https://gist.githubusercontent.com/emarco177/0d6a3f93dd06634d95e46a2782ed7490/raw/fad4d7a87e3e934ad52ba2a968bad9eb45128665/eden-marco.json";
-        response = requests.get(
-            linkedin_profile_url,
-            timeout=10
-        )
+        linkedin_profile_url = "https://gist.githubusercontent.com/emarco177/0d6a3f93dd06634d95e46a2782ed7490/raw/fad4d7a87e3e934ad52ba2a968bad9eb45128665/eden-marco.json"
+        response = requests.get(linkedin_profile_url, timeout=10)
     else:
         api_endpoint = "https://nubela.co/proxycurl/api/v2/linkedin"
         header_dic = {"Authorization": f'Bearer {os.environ.get("PROXYCURL_API_KEY")}'}
@@ -21,7 +19,7 @@ def scrape_linkedin_profile(linkedin_profile_url: str, mock: bool = False):
             api_endpoint,
             params={"url": linkedin_profile_url},
             headers=header_dic,
-            timeout=10
+            timeout=10,
         )
 
     data = response.json()
@@ -29,7 +27,7 @@ def scrape_linkedin_profile(linkedin_profile_url: str, mock: bool = False):
         k: v
         for k, v in data.items()
         if v not in ([], "", "", None)
-           and k not in ["people_also_viewed", "certifications"]
+        and k not in ["people_also_viewed", "certifications"]
     }
     if data.get("groups"):
         for group_dict in data.get("groups"):
@@ -37,10 +35,10 @@ def scrape_linkedin_profile(linkedin_profile_url: str, mock: bool = False):
 
     return data
 
+
 if __name__ == "__main__":
     print(
         scrape_linkedin_profile(
-            linkedin_profile_url="https://www.linkedin.com/in/eden-marco/",
-            mock=True
+            linkedin_profile_url="https://www.linkedin.com/in/eden-marco/", mock=True
         )
     )
